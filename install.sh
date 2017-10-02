4f959915e7edbdab93887f7dcc5a590e60d30ca7
Marco Giraudi (Old Snapo) <mgiraudi@soluzioninrete.it>
2017-10-02 16:33:13 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IFS_BK=$IFS
+IFS='
+'
+
+PLUGIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GW_DIR=${PLUGIN_DIR%/*/*}
+while [ ! -f "${GW_DIR}/config/mapfile.outputformats.inc" ]
+do
+	echo "Insert gisclient 3 installation directory:"
+	read GW_DIR
+done	
+
+for i in $(ls -R $PLUGIN_DIR | awk ' /:$/&&f{s=$0;f=0} /:$/&&!f{sub(/:$/,"");s=$0;f=1;next} NF&&f{ print s"/"$0 }')
+do 
+	PLUGIN_ELEM=${i#$PLUGIN_DIR}
+	echo $PLUGIN_ELEM
+	if [  "${PLUGIN_ELEM}" != '/install.sh' ] && [ "${PLUGIN_ELEM}" != '/uninstall.sh' ]
+	then
+		if [ ! -e "${GW_DIR}${PLUGIN_ELEM}" ] || [ -L "${GW_DIR}${PLUGIN_ELEM}" ]
+		then
+			ln -sf $i ${GW_DIR}${PLUGIN_ELEM}
+		fi
+	fi
+done
+
+IFS=$IFS_BK
---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IFS_BK=$IFS
+IFS='
+'
+
+PLUGIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GW_DIR=${PLUGIN_DIR%/*/*}
+while [ ! -f "${GW_DIR}/config/mapfile.outputformats.inc" ]
+do
+	echo "Insert gisclient 3 installation directory:"
+	read GW_DIR
+done	
+
+for i in $(ls -R $PLUGIN_DIR | awk ' /:$/&&f{s=$0;f=0} /:$/&&!f{sub(/:$/,"");s=$0;f=1;next} NF&&f{ print s"/"$0 }')
+do 
+	PLUGIN_ELEM=${i#$PLUGIN_DIR}
+	echo $PLUGIN_ELEM
+	[ -L "${GW_DIR}${PLUGIN_ELEM}" ] && { rm ${GW_DIR}${PLUGIN_ELEM}; }
+done
+
+IFS=$IFS_BK
